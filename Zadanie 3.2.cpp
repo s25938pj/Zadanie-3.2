042f7f25fd8223967a00750ab1d587905b260cf6
Jakub <Jakub@KWASNIAK-J>
2021-11-13 19:01:14 +0100

    Dodaj pliki projektów.

---
+++ Zadanie 3.2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+	int operacja = 1;
+	char znak = ' ';
+
+	cout << "Po wcisnieciu klawisza 't' twoj program zakonczy zadanie " << endl ;
+
+	while (operacja) {
+
+		znak = _getch();
+		cout << znak;
+		if (znak == 't' || znak == 'T')
+		{
+			break;
+		}
+	}
+}
